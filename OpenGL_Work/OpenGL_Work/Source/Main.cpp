63a533e7ffa86f75969bd0a4c1faf283b3877985
Roman Avg <romananaa25@gmail.com>
2022-05-09 21:00:18 +0300

    Changes

--- OpenGL_Work/OpenGL_Work/Source/Main.cpp
+++ OpenGL_Work/OpenGL_Work/Source/Main.cpp
@@ -1,6 +1,10 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+void Right_Side() 
+{
+
+}
 int main(void)
 {
     GLFWwindow* window;
@@ -13,101 +17,126 @@ int main(void)
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window); 
+
+    bool side = 0;
+    int count = 0;
     while (!glfwWindowShouldClose(window))
     {
-        float ratio;
-        int width, height;
+        if (!side) {
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_BLEND);
 
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = (float)width / (float)height;
+            float ratio;
+            int width, height;
 
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT); 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float)glfwGetTime() * 50.f, 0.f, 1.f, 0.f);
+            glfwGetFramebufferSize(window, &width, &height);
+            ratio = (float)width / (float)height;
 
-        /*glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);*/
-
-        /*glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();*/
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(0.f, 0.f, 1.f);
-        glVertex3f(1.f, -1.f, 0.f);
-        glVertex3f(1.f, 1.f, 0.f);
-        glColor3f(1.f, 1.f, 0.f);
-        glVertex3f(0.f, 0.f, 1.f);
-        glVertex3f(1.f, 1.f, 0.f);
-        glVertex3f(-1.f, 1.f, 0.f);
-        glColor3f(1.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.f, 1.f);
-        glVertex3f(-1.f, 1.f, 0.f);
-        glVertex3f(-1.f, -1.f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.f, 0.f, 1.f);
-        glVertex3f(-1.f, -1.f, 0.f);
-        glVertex3f(1.f, -1.f, 0.f);
-        glEnd();
-
-        glBegin(GL_QUADS);        // Draw The Cube Using quads
-        glColor3f(1.0f, 1.0f, 1.0f);    // Color Blue
-        glVertex3f(-1.0f, 1.0f, 0.0f);    // Top Right Of The Quad (Top)
-        glVertex3f(-1.0f, -1.0f, 0.0f);    // Top Left Of The Quad (Top)
-        glVertex3f(1.0f, -1.0f, 0.0f);    // Bottom Left Of The Quad (Top)
-        glVertex3f(1.0f, 1.0f, 0.0f);
-        glEnd();
-
-        //glBegin(GL_QUADS);        // Draw The Cube Using quads
-        //glColor3f(0.0f, 1.0f, 0.0f);    // Color Blue
-        //glVertex3f(1.0f, 1.0f, -1.0f);    // Top Right Of The Quad (Top)
-        //glVertex3f(-1.0f, 1.0f, -1.0f);    // Top Left Of The Quad (Top)
-        //glVertex3f(-1.0f, 1.0f, 1.0f);    // Bottom Left Of The Quad (Top)
-        //glVertex3f(1.0f, 1.0f, 1.0f);    // Bottom Right Of The Quad (Top)
-        //glColor3f(1.0f, 0.5f, 0.0f);    // Color Orange
-        //glVertex3f(1.0f, -1.0f, 1.0f);    // Top Right Of The Quad (Bottom)
-        //glVertex3f(-1.0f, -1.0f, 1.0f);    // Top Left Of The Quad (Bottom)
-        //glVertex3f(-1.0f, -1.0f, -1.0f);    // Bottom Left Of The Quad (Bottom)
-        //glVertex3f(1.0f, -1.0f, -1.0f);    // Bottom Right Of The Quad (Bottom)
-        //glColor3f(1.0f, 0.0f, 0.0f);    // Color Red    
-        //glVertex3f(1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Front)
-        //glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Front)
-        //glVertex3f(-1.0f, -1.0f, 1.0f);    // Bottom Left Of The Quad (Front)
-        //glVertex3f(1.0f, -1.0f, 1.0f);    // Bottom Right Of The Quad (Front)
-        //glColor3f(1.0f, 1.0f, 0.0f);    // Color Yellow
-        //glVertex3f(1.0f, -1.0f, -1.0f);    // Top Right Of The Quad (Back)
-        //glVertex3f(-1.0f, -1.0f, -1.0f);    // Top Left Of The Quad (Back)
-        //glVertex3f(-1.0f, 1.0f, -1.0f);    // Bottom Left Of The Quad (Back)
-        //glVertex3f(1.0f, 1.0f, -1.0f);    // Bottom Right Of The Quad (Back)
-        //glColor3f(0.0f, 0.0f, 1.0f);    // Color Blue
-        //glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Left)
-        //glVertex3f(-1.0f, 1.0f, -1.0f);    // Top Left Of The Quad (Left)
-        //glVertex3f(-1.0f, -1.0f, -1.0f);    // Bottom Left Of The Quad (Left)
-        //glVertex3f(-1.0f, -1.0f, 1.0f);    // Bottom Right Of The Quad (Left)
-        //glColor3f(1.0f, 0.0f, 1.0f);    // Color Violet
-        //glVertex3f(1.0f, 1.0f, -1.0f);    // Top Right Of The Quad (Right)
-        //glVertex3f(1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Right)
-        //glVertex3f(1.0f, -1.0f, 1.0f);    // Bottom Left Of The Quad (Right)
-        //glVertex3f(1.0f, -1.0f, -1.0f);    // Bottom Right Of The Quad (Right)
-        //glEnd();            // End Drawing The Cube
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+            glViewport(0, 0, width, height);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            glRotatef(/*(float)glfwGetTime() **/count/** 50.f*/, 0.f, 1.f, 0.f);
 
 
+            glBegin(GL_TRIANGLES);
+            glColor4f(0.f, 1.f, 0.f, 1.0f); //green
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+            glVertex3f(1.f, -1.f, 0.f);
+            glColor4f(1.f, 1.f, 0.f, 1.0f); //yellow
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(1.f, 1.f, 0.f);
+            glVertex3f(-1.f, 1.f, 0.f);
+
+            glColor4f(1.f, 0.f, 0.f, 0.5f); //red
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(1.f, -1.f, 0.f);
+            glVertex3f(1.f, 1.f, 0.f);
+            glColor4f(1.f, 0.f, 1.f, 0.5f); //purpule
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(-1.f, 1.f, 0.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+
+
+            glEnd();
+
+            glBegin(GL_QUADS);        // Draw The Cube Using quads
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);    // Color white
+            glVertex3f(-1.0f, 1.0f, 0.0f);    // Top Right Of The Quad (Top)
+            glVertex3f(-1.0f, -1.0f, 0.0f);    // Top Left Of The Quad (Top)
+            glVertex3f(1.0f, -1.0f, 0.0f);    // Bottom Left Of The Quad (Top)
+            glVertex3f(1.0f, 1.0f, 0.0f);
+            glEnd();
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        else {
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_BLEND);
+
+            float ratio;
+            int width, height;
+
+            glfwGetFramebufferSize(window, &width, &height);
+            ratio = (float)width / (float)height;
+
+            glViewport(0, 0, width, height);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            glRotatef(/*(float)glfwGetTime() **/count /** 50.f*/, 0.f, 1.f, 0.f);
+
+
+            glBegin(GL_QUADS);        // Draw The Cube Using quads
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);    // Color white
+            glVertex3f(-1.0f, 1.0f, 0.0f);    // Top Right Of The Quad (Top)
+            glVertex3f(-1.0f, -1.0f, 0.0f);    // Top Left Of The Quad (Top)
+            glVertex3f(1.0f, -1.0f, 0.0f);    // Bottom Left Of The Quad (Top)
+            glVertex3f(1.0f, 1.0f, 0.0f);
+            glEnd();
+
+            glBegin(GL_TRIANGLES);
+            glColor4f(0.f, 1.f, 0.f, 1.0f); //green
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+            glVertex3f(1.f, -1.f, 0.f);
+            glColor4f(1.f, 1.f, 0.f, 1.0f); //yellow
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(1.f, 1.f, 0.f);
+            glVertex3f(-1.f, 1.f, 0.f);
+
+            glColor4f(1.f, 0.f, 1.f, 0.5f); //purpule
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(-1.f, 1.f, 0.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+            glColor4f(1.f, 0.f, 0.f, 0.5f); //red
+            glVertex3f(0.f, 0.f, 1.f);
+            glVertex3f(1.f, -1.f, 0.f);
+            glVertex3f(1.f, 1.f, 0.f);
+
+            glEnd();            
+
+            glBegin(GL_LINE);
+            glVertex3f(0.0f, 0.0f, 0.0f);
+            glVertex3f(0.0f, 0.0f, 1.0f);
+            glEnd();
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        count++;
+        if(count%90==0)
+            side = !side;
     }
     glfwDestroyWindow(window);
     glfwTerminate();
